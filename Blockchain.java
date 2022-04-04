201463df917a4d6c7318e80070636ceff449cd93
Oudahani <102731771+Oudahani@users.noreply.github.com>
2022-04-04 23:45:15 +0300

    Create Blockchain.java

---
+++ Blockchain.java
@@ -0,0 +1,66 @@
+public class Block {
+    private String hash;
+    private String previousHash;
+    private String data;
+    private long timeStamp;
+    private int nonce;
+ 
+    public Block(String data, String previousHash, long timeStamp) {
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = timeStamp;
+        this.hash = calculateBlockHash();
+    }
+    public String calculateBlockHash() {
+    String dataToHash = previousHash 
+      + Long.toString(timeStamp) 
+      + Integer.toString(nonce) 
+      + data;
+    MessageDigest digest = null;
+    byte[] bytes = null;
+    try {
+        digest = MessageDigest.getInstance("SHA-256");
+        bytes = digest.digest(dataToHash.getBytes(UTF_8));
+    } catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
+        logger.log(Level.SEVERE, ex.getMessage());
+    }
+    StringBuffer buffer = new StringBuffer();
+    for (byte b : bytes) {
+        buffer.append(String.format("%02x", b));
+    }
+    return buffer.toString();
+}
+public String mineBlock(int prefix) {
+    String prefixString = new String(new char[prefix]).replace('\0', '0');
+    while (!hash.substring(0, prefix).equals(prefixString)) {
+        nonce++;
+        hash = calculateBlockHash();
+    }
+    return hash;
+}
+List<Block> blockchain = new ArrayList<>();
+int prefix = 4;
+String prefixString = new String(new char[prefix]).replace('\0', '0');
+@Test
+public void givenBlockchain_whenNewBlockAdded_thenSuccess() {
+    Block newBlock = new Block(
+      "The is a New Block.", 
+      blockchain.get(blockchain.size() - 1).getHash(),
+      new Date().getTime());
+    newBlock.mineBlock(prefix);
+    assertTrue(newBlock.getHash().substring(0, prefix).equals(prefixString));
+    blockchain.add(newBlock);
+}
+@Test
+public void givenBlockchain_whenValidated_thenSuccess() {
+    boolean flag = true;
+    for (int i = 0; i < blockchain.size(); i++) {
+        String previousHash = i==0 ? "0" : blockchain.get(i - 1).getHash();
+        flag = blockchain.get(i).getHash().equals(blockchain.get(i).calculateBlockHash())
+          && previousHash.equals(blockchain.get(i).getPreviousHash())
+          && blockchain.get(i).getHash().substring(0, prefix).equals(prefixString);
+            if (!flag) break;
+    }
+    assertTrue(flag);
+}
+}
